492e770859d26d46c5156fdd4abfcd4e52483840
Vassily Delmas <vassily.delmas@cegedim.com>
2023-02-06 10:41:59 +0100

    quelques trucs testés

---
+++ prog_sys/App/feature.cs
@@ -0,0 +1,33 @@
+﻿namespace EasySafe;
+
+public class feature
+{
+    public void CreateLog(String logPath)
+    {
+        logPath += "/log.json";
+        try
+        {
+            if (!File.Exists(logPath))
+                File.Create(logPath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erreur lors de la création du fichier log : " + ex.Message);
+        }
+    }
+    
+    public void CreateState(String statePath)
+    {
+        statePath += "/state.json";
+        try
+        {
+            if (!File.Exists(statePath))
+                File.Create(statePath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erreur lors de la création du fichier state : " + ex.Message);
+        }
+    }
+
+}
