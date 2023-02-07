ee0249afc57d8764c23515b4a9ea8f940b5955de
TimDSi <theotime.dsilva1505@gmail.com>
2023-02-07 11:30:09 +0100

    Creation of Main with 2 languages

--- prog_sys/App/Program.cs
+++ prog_sys/App/Program.cs
@@ -1,4 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Data;
+using System.Globalization;
 
-Console.WriteLine("Hello, World!");
-Console.ReadLine("input : ");
+public class Program
+{
+
+    public static string readCommand(string message)
+    {
+        Console.WriteLine(message);
+        return Console.ReadLine();
+    }
+
+    static void Main(string[] args)
+    {
+        Language selectedLanguage = new Language(readCommand("Français : 1 , English : 2"));
+
+        string command = readCommand(selectedLanguage.defaultMessage());
+
+        while (command != "exit")
+        {
+            switch (command)
+            {
+                case "save":
+                    string saveName = readCommand(selectedLanguage.saveNameMessage());
+                    string originPath = readCommand(selectedLanguage.originPathMessage());
+                    string targetPath = readCommand(selectedLanguage.targetPathMessage());
+                    int numberOfFilesToCopy = Int32.Parse(readCommand(selectedLanguage.numberOfFilesToSaveMessage()));
+                    for (int i = 0; i < numberOfFilesToCopy; i++)
+                    {
+                        string fileName = readCommand(selectedLanguage.fileNameMessage());
+                        //lancer une sauvegarde
+                    }
+                    break;
+                case "language":
+                    selectedLanguage = new Language(readCommand("Français : 1 , English : 2"));
+                    break;
+                default:
+                    Console.WriteLine("unknown command");
+                    break;
+            }
+            command = readCommand(selectedLanguage.defaultMessage());
+        }
+    }
+}
