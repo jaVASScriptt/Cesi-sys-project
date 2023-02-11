13ff76f5083c5559ed10a1478dbcd1323156cf65
Timburio <Timburio@DESKTOP-QEE359B>
2023-02-11 17:11:37 +0100

    Creating the factory and the Interface

---
+++ prog_sys/App/Save/FactorySave.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controler
+{
+    public class FactorySave
+    {
+        public Save getSave(string saveName, string originPath, string targetPath, string type)
+        {
+            Save newSave;
+            switch (type) {
+                case "Complete":
+                    newSave = new DifferentialSave(saveName, originPath, targetPath);
+                    break
+                case "Differential":
+                    newSave = new CompleteSave(saveName, originPath, targetPath);
+                    break;
+                default:
+                    newSave = null;
+                    break;
+            }
+            return newSave;
+        }
+    }
+}
