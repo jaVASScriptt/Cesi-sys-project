27aad18ea967d86f267782c7e74b62b903e9de81
Vassily Delmas <vassily.delmas@cegedim.com>
2023-02-12 02:05:23 +0100

    fichier erreur que l'on peux utiliser pour gérer les erreurs

---
+++ prog_sys/App/Features/utils/Errors.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp2.Features.utils;
+
+public class Errors
+{
+    public static Boolean outOfRange(int index, List<object> l)
+    {
+        if (index > 0 && index < l.Count - 1)
+            return false;
+        Console.WriteLine("Indice cesi en dehors de la liste");
+        return true;
+    }
+    
+}
