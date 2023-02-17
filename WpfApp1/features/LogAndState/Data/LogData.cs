736d13ebd5d2c36b19965ef0aa18f28cf9493307
Vassily Delmas <vassily.delmas@cegedim.com>
2023-02-17 14:23:29 +0100

    correction wpf

---
+++ WpfApp1/features/LogAndState/Data/LogData.cs
@@ -0,0 +1,12 @@
+﻿namespace EasySafe;
+
+public class LogData
+{
+    public string Name { get; set; }
+    public string SourceFilePath { get; set; }
+    public string TargetFilePath { get; set; }
+    public string success { get; set; }
+    public long FileSize { get; set; }
+    public double FileTransferTime { get; set; }
+    public string Time { get; set; }
+}
---
+++ WpfApp1/features/LogAndState/Data/TaskData.cs
@@ -0,0 +1,29 @@
+﻿namespace EasySafe;
+
+public class TaskData
+{
+    public string Name { get; set; }
+    public string SourceFilePath { get; set; }
+    public string TargetFilePath { get; set; }
+    public string State { get; set; }
+    public int TotalFilesToCopy { get; set; }
+    public long TotalFilesSize { get; set; }
+    public int NbFilesLeftToDo { get; set; }
+    public int Progression { get; set; }
+    public string Type { get; set; }
+    public string LastUsed { get; set; }
+    
+    public TaskData()
+    {
+        Name = "";
+        SourceFilePath = "";
+        TargetFilePath = "";
+        State = "END";
+        TotalFilesToCopy = 0;
+        TotalFilesSize = 0;
+        NbFilesLeftToDo = 0;
+        Progression = 0;
+        Type = "complete";
+        LastUsed = "";
+    }
+}
