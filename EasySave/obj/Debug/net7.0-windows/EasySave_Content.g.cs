4a399f80eecd9fe68c2aec4d8516a630514f2bf7
Vassily Delmas <vassily.delmas@cegedim.com>
2023-02-21 22:36:42 +0100

    template de base finis et satisfaisante ( sauf les paramètres )

--- EasySave/obj/Debug/net7.0-windows/EasySave_Content.g.cs
+++ EasySave/obj/Debug/net7.0-windows/EasySave_Content.g.cs
@@ -11,5 +11,8 @@
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("wpf/images/exit_logo.png")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("wpf/images/drapeau-anglais.png")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("wpf/images/drapeau-france.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("wpf/images/edit.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("wpf/images/start.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("wpf/images/del.png")]
 
 
