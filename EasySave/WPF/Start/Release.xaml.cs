b5979c4b4b6fdd6e380579a726288830a223368d
andre <andrew.hiver@gmail.com>
2023-02-23 15:23:09 +0100

    Add realease Note pour une fois

---
+++ EasySave/WPF/Start/Release.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace EasySave.WPF.Start;
+
+public partial class Release : Page
+{
+    public Release()
+    {
+        InitializeComponent();
+
+        ReleaseTitle.Text = LanguageTool.get("ReleaseTitle");
+        V1Text.Text = LanguageTool.get("V1Text");
+        V1_1Text.Text = LanguageTool.get("V1_1Text");
+        V2Text.Text = LanguageTool.get("V2Text");
+        V3Text.Text = LanguageTool.get("V3Text");
+    }
+}
+
+
+
+
